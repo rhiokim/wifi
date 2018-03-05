0db33c4332d27b877defeb56f793171570fd684f
Rhio Kim <rhio.kim@gmail.com>
2018-03-05 18:37:14 +0900

    feat: support cli mode

---
+++ bin/index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+'use strict';
+
+const meow = require('meow');
+const wifi = require('../libs/wifi');
+
+const cli = meow(`
+    Usage
+      $ office <input>
+
+    Sub Command
+      $ office wifi   Search SSID
+
+    Options
+      --filter, -f  Filter by
+
+    Examples
+      $ office wifi --auto-connect
+      🌈 unicorns 🌈
+`, {
+    flags: {
+      'auto-connect': {
+        type: 'boolean',
+        alias: 'a',
+        default: false
+      },
+      filter: {
+        type: 'string',
+        alias: 'f'
+      },
+      sort: {
+        type: 'string',
+        alias: 's',
+        default: 'desc'
+      }
+    }
+});
+
+switch(cli.input[0]) {
+  case 'git':
+  break;
+  case 'wifi':
+    wifi(cli.flags)
+  break;
+}
+
---
+++ libs/wifi.js
@@ -0,0 +1,60 @@
+const prompt = require('prompts');
+const wifi = require('node-wifi');
+const ora = require('ora');
+const chalk = require('chalk')
+
+module.exports = (flags) => {
+  const spinner = ora('Searching Best Wifi for You...').start();
+  spinner.color = 'yellow';
+
+  wifi.init({
+    iface: null
+  })
+
+  wifi.scan(async (err, networks) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    let res = networks
+      .sort((a, b) => b.signal_level - a.signal_level);
+
+    res = res.map(net => {
+      return {
+        ...net,
+        title: `${net.ssid} ${chalk.grey(net.signal_level)}`,
+        value: net.ssid
+      }
+    })
+
+    spinner.stop();
+
+    const choice = await prompt([
+      {
+        type: 'select',
+        name: 'ssid',
+        message: 'Pick a Wifi',
+        choices: res
+      },
+      {
+        type: 'password',
+        name: 'password',
+        message: 'Tell me wifi password',
+        initial: '',
+      }
+    ]);
+
+    spinner.text = `Connecting into ${choice.ssid} Wifi`;
+    spinner.start();
+
+    wifi.connect({ ssid : choice.ssid, password : choice.password }, function(err) {
+      if (err) {
+        console.log(err);
+        throw err
+      }
+
+      spinner.succeed(`Connected ${choice.ssid} SSID`);
+    });
+  });
+}
